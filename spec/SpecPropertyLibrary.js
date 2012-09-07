037ac3280e78786d15037c22b2b5c3608a135796
John Vrbanac <john.vrbanac@linux.com>
2012-09-07 00:29:19 -0500

    Fixing indention and adding path validity test.

--- spec/SpecPropertyLibrary.js
+++ spec/SpecPropertyLibrary.js
@@ -1,11 +1,22 @@
 describe("PropertyLibrary", function() {
-  var libary;
+	var libary;
 
-  beforeEach(function() {
-    library = new PropertyLibrary();
-  });
+	beforeEach(function() {
+		library = new PropertyLibrary();
+	});
 
-  it("should be able to instantiate", function() {
-    expect(library).not.toBeNull();
-  });
+	it("should be able to instantiate", function() {
+		expect(library).not.toBeNull();
+	});
+
+	describe("Utilities", function() {
+
+		it("can determine if a path is valid", function() {
+			expect(library.isPathValid("//base/test")).toBe(true);
+
+			expect(library.isPathValid("//base")).toBe(false);
+			expect(library.isPathValid("///")).toBe(false);
+			expect(library.isPathValid("/")).toBe(false);
+		});
+	});
 });
